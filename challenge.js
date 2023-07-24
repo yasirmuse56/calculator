24200a45c801767840bb6ec0cbef1a10b7f86931
h. mikail <>
2023-07-24 03:36:14 -0700

    calculator ex sol

--- challenge.js
+++ challenge.js
@@ -1,2 +1,20 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+function calculator(num1, num2, operator) {
+    if (operator === "+") {
+      return num1 + num2;
+    } else if (operator === "-") {
+      return num1 - num2;
+    } else if (operator === "*") {
+      return num1 * num2;
+    } else if (operator === "/") {
+      return num1 / num2;
+    } else {
+      return "Invalid operator";
+    }
+  }
+  
+  // Test cases
+  console.log(calculator(5, 7, "+")); // Output: 8
+  console.log(calculator(3, 6, "-")); // Output: 2
+  console.log(calculator(2, 3, "*")); // Output: 15
+  console.log(calculator(77, 3, "/")); // Output: 1.6666666666666667
+  console.log(calculator(5, 3, "%")); // Output: Invalid operator
